868e4811073d27e05933e4cf1678b489f79f72d6
Cauan Nascimento <caunas1337@proton.me>
2026-03-10 15:17:29 -0300

    Aula 3

---
+++ product/viewsets/product_viewset.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    serializer_class = Product Ignite
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+
