d2cb6c4172a55adec5d3cc9cb2b72cd599e949fd
Cauan Nascimento <caunas1337@proton.me>
2026-03-19 19:30:49 -0300

    Aula 6

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django_extensions",
     "order",
     "product",
-    "debug_toolbar"
+    "debug_toolbar",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         ]
 }
 
