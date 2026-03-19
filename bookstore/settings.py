baaad8e639766298b7cced71711d72d017ff954c
Cauan Nascimento <caunas1337@proton.me>
2026-03-19 18:55:23 -0300

    Aula 3

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -127,7 +127,11 @@ STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        ]
 }
 
 INTERNAL_IPS = [
