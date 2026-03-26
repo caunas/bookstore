43f9c874d8c75e79d393edb77897b6090812bd51
Cauan Nascimento <caunas1337@proton.me>
2026-03-26 10:44:31 -0300

    Remoção da requisição de autenticação da viewset de produto

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+#from rest_framework.permissions import IsAuthenticated
+#from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 
 from product.models import Product
@@ -8,8 +8,8 @@ from product.serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authenticatioon_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    #authenticatioon_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    #permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
